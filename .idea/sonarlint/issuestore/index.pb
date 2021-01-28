3206bbb8781a2074f433548979b2ba7980d0a9b0
Naman <namannigam12@gmail.com>
2021-01-28 16:44:05 +0530

    add resources to perform join

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+Y
+)src/main/java/SimpleSparkApplication.java,f/2/f2bf391f348f699569e4adfe475b114923795558
--- src/main/java/SimpleSparkApplication.java
+++ src/main/java/SimpleSparkApplication.java
@@ -1,23 +1,23 @@
-import org.apache.spark.api.java.function.FilterFunction;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+
+import java.util.Arrays;
+import java.util.List;
 
 // -Dio.netty.tryReflectionSetAccessible=true is required additionally for Apache Arrow library
 public class SimpleSparkApplication {
 
     public static void main(String[] args) {
-        String logFile = "/Users/naman.nigam/Downloads/spark-3.0.1-bin-hadoop3.2/README.md"; // Should be some file on your system
         SparkSession spark = SparkSession.builder()
                 .appName("Simple Application")
                 .getOrCreate();
         spark.sparkContext().setLogLevel("DEBUG");
-        Dataset<String> logData = spark.read().textFile(logFile).cache();
 
-        long numAs = logData.filter((FilterFunction<String>) s -> s.contains("a")).count();
-        long numBs = logData.filter((FilterFunction<String>) s -> s.contains("b")).count();
-
-        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
-
+        List<String> data = Arrays.asList("dev, engg, 10000", "karthik, engg, 20000");
+        Dataset<Row> dataframe = spark.sqlContext().createDataset(data, Encoders.STRING()).toDF();
+        dataframe.show();
         spark.stop();
     }
 }
